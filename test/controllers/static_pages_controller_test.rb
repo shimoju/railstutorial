552a5aa470e766b0cf3e4be7438c3e43b9c586e7
ogidow <syuta.ogido@gmail.com>
2016-06-15 10:38:26 +0900

    static_pages#aboutのルーティグ追加

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -11,7 +11,7 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-	test "should get help" do
+	test "should get about" do
 		get :about
 		assert_response :success
 	end
