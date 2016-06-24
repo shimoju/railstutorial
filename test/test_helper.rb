3e8cb57c7c9da630786227ced88f9e3a4955e9c2
ogidow <syuta.ogido@gmail.com>
2016-06-24 15:05:50 +0900

    remember meのテスト追加

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,5 @@
 ENV['RAILS_ENV'] ||= 'test'
+
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require "minitest/reporters"
@@ -12,5 +13,19 @@ class ActiveSupport::TestCase
     !session[:user_id].nil?
   end
 
-  # Add more helper methods to be used by all tests here...
+  def log_in_as(user, options ={})
+    password = options[:password] || 'password'
+    remember_me = options[:remember_me] || '1'
+    if integration_test?
+      post login_path, session:{email: user.email, password: password, remember_me: remember_me}
+    else
+      session[:user_id] = user.id
+    end
+  end
+
+  private
+
+  def integration_test?
+    defined?(post_via_redirect)
+  end
 end
