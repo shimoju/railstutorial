82f39074b1cc0865e684282629d668aab0d2622a
ogidow <syuta.ogido@gmail.com>
2016-07-06 12:19:10 +0900

    エクササイズ3

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -10,6 +10,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
+    assert_select 'input[type=file]'
 
     #空の投稿
     assert_no_difference 'Micropost.count' do
@@ -19,9 +20,14 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     
     #投稿成功
     content = "This micropost really ties the room together"
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, micropost: {content: content}
+      post microposts_path, micropost: {content: content, picture: picture}
     end
+
+    #micropostオブジェクトをコントローラから読んできて、画像を持ってるか確かめる
+    assert assigns(:micropost).picture?
+
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
