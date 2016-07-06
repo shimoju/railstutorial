42c9cff80fc4cef6593b28804238fbb94c8610bc
ogidow <syuta.ogido@gmail.com>
2016-07-06 14:32:51 +0900

    relationshipsのバリデートとテスト書いた

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
-  has_many :active_relationship. class_name: "Relationship",
-           :foreign_key: "follower_id",
-           :dependent :destroy
+  has_many :active_relationship, class_name: "Relationship",
+           foreign_key: "follower_id",
+           dependent: :destroy
 
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
