9b20441b2ec55601c74edac0a588906bbac1d518
ogidow <syuta.ogido@gmail.com>
2016-07-11 12:54:25 +0900

    api_usersコントローラのテスト

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -4,7 +4,6 @@ class Api::UsersController < ApplicationController
   def index
     users = User.where(activated: true).paginate(page: params[:page])
     build_json = Jbuilder.encode do |json|
-      json.page params[:page]
       json.users users
     end
     render json: build_json
@@ -21,10 +20,7 @@ class Api::UsersController < ApplicationController
         json.email user.email
         json.activated user.activated
         json.icon_url gravatar_for(user, url: true)
-        json.microposts do
-          json.page page
-          json.contents microposts
-        end
+        json.microposts microposts
       end
     end
 
@@ -32,22 +28,22 @@ class Api::UsersController < ApplicationController
   end
 
   def create
-    user = User.new(JSON.parse(request.body.read, {symbolize_names: true}))
+    user = User.new(user_params)
     message = ""
 
     if user.save
       user.send_activation_email
       build_json = Jbuilder.encode do |json|
-        json.status 201
         json.message "Created"
       end
       render json: build_json, status: 201
     else
       build_json = Jbuilder.encode do |json|
-        json.status 400
-        json.message user.error_messages
+        json.message "Validation Failed"
+        json.errors user.errors.messages
+
       end
-      render json: build_json, status: 400
+      render json: build_json, status: 500
     end
   end
 
@@ -61,5 +57,10 @@ class Api::UsersController < ApplicationController
   end
 
   def followers
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- test/controllers/api/users_controller_test.rb
+++ test/controllers/api/users_controller_test.rb
@@ -1,7 +1,55 @@
 require 'test_helper'
 
 class Api::UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:michael)
+    ActionMailer::Base.deliveries.clear
+  end
+
+  test "user一覧の取得" do
+    get :index
+
+    params = JSON.parse(response.body, symbolize_names: true)
+    first_page_users = User.where(activated: true).paginate(page: 1)
+    users_names = params[:users].map{|user| user[:name]}
+
+    first_page_users.each do |user|
+      assert users_names.include?(user[:name])
+    end
+  end
+
+  test "user情報の取得" do
+    get :show, id: @user
+    assert_response :success
+
+    params = JSON.parse(response.body, symbolize_names: true) 
+    assert_not_nil params[:user]
+    assert_equal @user[:name], params[:user][:name]
+    assert_equal @user[:email], params[:user][:email]
+  end
+
+  test "ユーザのサインアップ(失敗)" do
+    assert_no_difference "User.count" do
+      post :create, user: {
+                            name: "",
+                            email: "user@invalid",
+                            password: "foo",
+                            password_confirmation: "bar"
+                          }
+    end
+    assert_response :internal_server_error
+  end
+
+  test "ユーザのサインアップ(成功)" do
+    assert_difference "User.count", 1 do
+      post :create, user: {
+                            name: "Example User",
+                            email: "user@example.com",
+                            password: "password",
+                            password_confirmation: "password"
+                          }
+    end
+    assert_response :created
+    assert_equal 1, ActionMailer::Base.deliveries.size
+  end
 end
