9fb94e5133498bf75513be18729bfa752382ff94
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-07-25 12:40:32 +0900

    アプリ名とGitリポジトリのURLを設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for current version of Capistrano
 lock '3.5.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'railstutorial'
+set :repo_url, 'https://github.com/ogidow/railstutorial.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
