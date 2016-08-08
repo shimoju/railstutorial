71770dafaccd211b789cc644f8ff0b5ef83f0f0b
ogidow <syuta.ogido@pepabo.com>
2016-08-08 13:55:36 +0900

    hostはall

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-server 'localhost',
+server 'all',
   user: 'deploy',
   roles: %w{app db web},
   ssh_options: {
