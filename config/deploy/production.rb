0d026e0c7cfd31cc0da2484ce434165d8d5a7251
ogidow <syuta.ogido@pepabo.com>
2016-08-09 14:56:47 +0900

    secret.ymlもシンボリックリンク

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -19,7 +19,7 @@ server 'all',
 
 set :rails_env, 'production'
 set :branch, 'master'
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 
 # role-based syntax
 # ==================
