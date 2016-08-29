7f43098039732e8561e640dcdd520d7e0594aec0
ogidow <syuta.ogido@pepabo.com>
2016-08-29 17:55:23 +0900

    webロールは使わない

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -9,7 +9,7 @@
 %w(app01 app02).each do |host|
   server host,
     user: 'rails',
-    roles: %w{app db web},
+    roles: %w{app db},
     ssh_options: {
       keys: %w(~/.ssh/id_ed25519),
       forward_agent: true,
