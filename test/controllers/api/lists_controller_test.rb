60d6f4129c8541629d85586dbd39a132e4582f01
ogidow <syuta.ogido@pepabo.com>
2016-09-08 16:57:55 +0900

    コントローラテストを書いた

--- test/controllers/api/lists_controller_test.rb
+++ test/controllers/api/lists_controller_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class Api::ListsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:michael)
+  end
+
+  test "create list with invalid data" do
+    assert_no_diffrence "List.count" do
+      post :create, format: :json, user: {
+        name: "",
+        user_id: ""
+      }
+    end
+    assert_response :unprocessable_entity
+  end
+
+  test "create list with valid data" do
+    assert_diffrence "List.count", 1 do
+      post :create, format: :json, user: {
+        name: "friends",
+        user_id: @user.id
+      }
+    end
+    assert_response :created
+  end
+
+  test "delete list with invalid data" do
+    assert_no_diffrence "List.count" do
+      delete :destroy, format: :json, user: {
+        name: "friends",
+        user_id: @user.id
+      }
+    end
+  end
 end
