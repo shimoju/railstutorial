4a18a9e89d8d78c16627dcf078c33ae305a6b0e5
ogidow <syuta.ogido@pepabo.com>
2016-09-08 17:22:07 +0900

    アソシエーション張った

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,4 @@
 class List < ActiveRecord::Base
   belongs_to :user
+  has_many :list_members, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
            dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+  has_many :lists, dependent: :destroy
 
 
   attr_accessor :remember_token, :activation_token, :reset_token
