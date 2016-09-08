b2db64588f53825809dbc87c3a23e24c5903638c
ogidow <syuta.ogido@pepabo.com>
2016-09-08 18:18:44 +0900

    バリデーション

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,7 @@
 class List < ActiveRecord::Base
   belongs_to :user
   has_many :list_members, dependent: :destroy
+  
+  validates :name, presence: true
+  validates :user_id, presence: true
 end
--- app/models/list_member.rb
+++ app/models/list_member.rb
@@ -1,4 +1,7 @@
 class ListMember < ActiveRecord::Base
   belongs_to :user
   belongs_to :list
+
+  validates :list_id, presence: true
+  validates :user_id, presence: true
 end
