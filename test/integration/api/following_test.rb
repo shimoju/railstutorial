d87116741b9e6e523970b42a65bbf97dae635df3
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-09-23 12:13:19 +0900

    テスト項目をリストアップ

--- test/integration/api/following_test.rb
+++ test/integration/api/following_test.rb
@@ -1,4 +1,18 @@
 require 'test_helper'
 
 class Api::FollowingTest < ActionDispatch::IntegrationTest
+  def setup
+    user = users(:michael)
+    token = user.generate_jwt
+    @headers = {"Authorization" => "Bearer #{token}"}
+  end
+
+  test "フォローできること" do
+  end
+
+  test "アンフォローできること" do
+  end
+
+  test "他のユーザーのフォローは操作できないこと" do
+  end
 end
