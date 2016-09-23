67dec1aeda5e650d00a88612255f336ff3f099c4
ogidow <syuta.ogido@pepabo.com>
2016-09-23 12:28:54 +0900

    同じ意味のインテグレーションテストが2個あったのでまとめる

--- test/integration/api/lists_create_test.rb
+++ test/integration/api/lists_create_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
-class Api::ListsCreateTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+class Api::ListCreateTest < ActionDispatch::IntegrationTest
+  def setup
+    user = users(:michael)
+    token = user.generate_jwt
+    @headers = {"Authorization" => "Bearer #{token}"}
+  end
+
+  test "正しい入力値でのリストの作成" do
+    list_name = "lover"
+    assert_difference "List.count", 1 do
+      post api_lists_path, {list: {name: list_name}}, @headers
+    end
+
+    assert_response :created
+    params = response_json
+
+    assert_equal params[:list][:name], list_name
+  end
 end
