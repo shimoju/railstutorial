b2c1a2b4fdec6da4a0e600f3a0bc29940e5441b7
ogidow <syuta.ogido@gmail.com>
2016-09-23 18:52:08 +0900

    メンバーズコントローラのテスト

--- test/controllers/api/lists/members_controller_test.rb
+++ test/controllers/api/lists/members_controller_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
-class Api::Lists::ListMembersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class Api::Lists::MembersControllerTest < ActionController::TestCase
+  def setup
+    @list = lists(:friend)
+    @user = users(:lana)
+    @member = users(:archer)
+  end
+
+  test "認証なしでリストのユーザ一覧を取得" do
+    get :index, format: :json, list_id: @list.id
+    assert_response :unauthorized
+  end
+
+  test "認証なしでリストにユーザを追加" do
+    post :create, format: :json, list_id: @list.id, user_id: @user.id
+    assert_response :unauthorized
+  end
+
+  test "認証なしでリストからユーザを削除" do
+    delete :destroy, format: :json, list_id: @list.id, id: @member.id
+    assert_response :unauthorized
+  end
 end
