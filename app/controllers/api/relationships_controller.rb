1f923522d428858723f44b89712017b54367215f
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-09-23 14:38:42 +0900

    strongpatametersで許可する

--- app/controllers/api/relationships_controller.rb
+++ app/controllers/api/relationships_controller.rb
@@ -2,7 +2,7 @@ class Api::RelationshipsController < Api::ApplicationController
   before_action :check_auth_token
 
   def create
-    @user = User.find(params[:followed_id])
+    @user = User.find(relationship_params[:followed_id])
     current_user.follow(@user)
     render status: :created
   end
@@ -12,4 +12,9 @@ class Api::RelationshipsController < Api::ApplicationController
     current_user.unfollow(@user)
     render nothing: true, status: :ok
   end
+
+  private
+    def relationship_params
+      params.require(:relationship).permit(:followed_id)
+    end
 end
