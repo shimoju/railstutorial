a3aabc6fbb701c45655b9f4d89f354fc9057a73f
ogidow <syuta.ogido@pepabo.com>
2016-09-23 15:09:46 +0900

    list.membersでリストに追加されているユーザ一覧を取得したい

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,7 @@
 class List < ActiveRecord::Base
   belongs_to :user
   has_many :list_members, dependent: :destroy
+  has_many :members, through: :list_members, source: :user
   
   validates :name, presence: true
   validates :user_id, presence: true
