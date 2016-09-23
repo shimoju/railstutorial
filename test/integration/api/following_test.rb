32d81c3ba1d4962f0eb195ce6200a3bad94991ac
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-09-23 12:19:39 +0900

    フォローできることのテスト

--- test/integration/api/following_test.rb
+++ test/integration/api/following_test.rb
@@ -2,12 +2,20 @@ require 'test_helper'
 
 class Api::FollowingTest < ActionDispatch::IntegrationTest
   def setup
-    user = users(:michael)
+    @user = users(:michael)
+    @other = users(:archer)
     token = user.generate_jwt
     @headers = {"Authorization" => "Bearer #{token}"}
   end
 
   test "フォローできること" do
+    assert_difference '@user.following.count', 1 do
+      post api_relationships_path, {relationship: {followed_id: @other.id}}, @headers
+    end
+    assert_response :created
+
+    params = response_json
+    assert_equal params[:relationship][:followed_id], @other.id
   end
 
   test "アンフォローできること" do
