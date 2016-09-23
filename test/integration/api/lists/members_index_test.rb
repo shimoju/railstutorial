8f01d30119cb2065d9d159e9b4a82edb3bf2e4e6
ogidow <syuta.ogido@pepabo.com>
2016-09-23 15:28:26 +0900

    lists/members#indexのテスト

---
+++ test/integration/api/lists/members_index_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class Api::Lists::MembersIndexTest < ActionDispatch::IntegrationTest
+  def setup
+    user = users(:michael)
+    token = user.generate_jwt
+    @headers = {"Authorization" => "Bearer #{token}"}
+    @list = lists(:friend)
+  end
+
+  test "リストに追加されているメンバー一覧を取得" do
+    get api_list_members_path(@list), {}, @headers
+
+    assert_response :ok
+
+    params = response_json
+    params[:members].each do |user|
+      %i(id name email).each do |element|
+        assert_not_nil micropost[element]
+      end
+    end
+  end
+end
