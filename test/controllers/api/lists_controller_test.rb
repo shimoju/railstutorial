b8fc436c8c61132d477d3c9d0c870399063a8d78
ogidow <syuta.ogido@gmail.com>
2016-09-23 18:33:40 +0900

    リストコントローラのテスト

--- test/controllers/api/lists_controller_test.rb
+++ test/controllers/api/lists_controller_test.rb
@@ -3,21 +3,32 @@ require 'test_helper'
 class Api::ListsControllerTest < ActionController::TestCase
   def setup
     @list = lists(:friend)
+    @user = users(:ogido)
   end
 
-  test "create list with invalid data" do
+  test "認証なしでリストを作成" do
     assert_no_difference "List.count" do
       post :create, format: :json, user: {
-        name: "",
-        user_id: ""
+        name: "test",
+        user_id: @user.id
       }
     end
     assert_response :unauthorized
   end
 
-  test "delete list with invalid data" do
+  test "認証なしでリストを削除" do
     assert_no_difference "List.count" do
       delete :destroy, format: :json, id: @list
     end
+    assert_response :unauthorized
+  end
+
+  test "認証なしでfeedの取得" do
+    get :feed, format: json, id: @list
+    assert_response :unauthorized
+  end
+
+  test "認証なしでリストの更新" do
+    patch :update, format: json, {list: {name: "ogidow"}, id: @list}
   end
 end
