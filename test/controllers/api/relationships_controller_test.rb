36b0ce4df2f6c6ce8dc7393bf995e9851a96f978
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-09-23 18:11:35 +0900

    可読性のため、認証しない場合のテストでもフォローする人のIDを渡しておく

--- test/controllers/api/relationships_controller_test.rb
+++ test/controllers/api/relationships_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class Api::RelationshipsControllerTest < ActionController::TestCase
   test "Relationshipの作成には認証が必要" do
     assert_no_difference 'Relationship.count' do
-      post :create, format: :json
+      post :create, format: :json, relationship: { followed_id: users(:archer).id}
     end
     assert_response :unauthorized
   end
