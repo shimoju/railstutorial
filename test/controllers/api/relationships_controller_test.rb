e8e2cb3ac0d3d323fbbc6553b7b32c40c5913798
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-09-23 11:56:16 +0900

    テスト作成

--- test/controllers/api/relationships_controller_test.rb
+++ test/controllers/api/relationships_controller_test.rb
@@ -1,14 +1,17 @@
 require 'test_helper'
 
 class Api::RelationshipsControllerTest < ActionController::TestCase
-  test "should get create" do
-    get :create
-    assert_response :success
+  test "Relationshipの作成には認証が必要" do
+    assert_no_difference 'Relationship.count' do
+      post :create, format: :json
+    end
+    assert_response :unauthorized
   end
 
-  test "should get destroy" do
-    get :destroy
-    assert_response :success
+  test "Relationshipの削除には認証が必要" do
+    assert_no_difference 'Relationship.count' do
+      delete :destroy, format: :json, id: relationships(:one)
+    end
+    assert_response :unauthorized
   end
-
 end
