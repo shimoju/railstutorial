1ed4912842ba0b91f8835ec7c29e6033b0aff473
ry023 <ry023.blue@gmail.com>
2016-09-26 12:32:56 +0900

    ユーザの持つmicroposts一覧表示のテスト

--- test/integration/api/user_microposts_test.rb
+++ test/integration/api/user_microposts_test.rb
@@ -32,4 +32,47 @@ class Api::UserMicropostsTest< ActionDispatch::IntegrationTest
     get api_user_microposts_path(@non_active_user), {}, @headers
     assert_response :forbidden
   end
+
+  test "userのmicrpost取得(最新10件)" do
+    get api_user_microposts_path(@user), {request_microposts: {count: 10}}, @headers
+    assert_response :success
+
+    # 35件取得する
+    assert_equal 10, response_json[:microposts].length
+  end
+
+  test "userのmicrpost取得(idが10より後のみ)" do
+    get api_user_microposts_path(@user), {request_microposts: {since_id: 10}}, @headers
+    assert_response :success
+
+    # 全micropostのidが10超であることを検証
+    response_json[:microposts].each do |micropost|
+      assert_operator micropost[:id], :>, 10
+    end
+  end
+
+  test "userのmicrpost取得(idが10以前のみ)" do
+    get api_user_microposts_path(@user), {request_microposts: {max_id: 10}}, @headers
+    assert_response :success
+
+    # 全micropostのidが10以下であることを検証
+    response_json[:microposts].each do |micropost|
+      assert_operator micropost[:id], :<=, 10
+    end
+  end
+
+  test "micropost取得(idが20以前かつ10より後のmicropostのみを5件取得)" do
+    # since_idを指定
+    get api_user_microposts_path(@user), {request_microposts: {count: 5, since_id: 10, max_id: 20}}, @headers
+    assert_response :success
+
+    # 全micropostのidが20以下10超であることを検証
+    response_json[:microposts].each do |micropost|
+      assert_operator micropost[:id], :<=, 20
+      assert_operator micropost[:id], :>, 10
+    end
+
+    # 5件取得する
+    assert_equal 5, response_json[:microposts].length
+  end
 end
