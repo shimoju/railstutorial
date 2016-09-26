2ea1880d4f65d4cd569cb996344cd0221f524c7f
ogidow <syuta.ogido@gmail.com>
2016-09-26 11:45:53 +0900

    list_memberモデルのテスト

--- test/models/list_member_test.rb
+++ test/models/list_member_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class ListMemberTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @list_member = ListMember.new(user_id: 1, list_id: 2)
+  end
+
+  test "正しい入力値" do
+    assert @list_member.valid?
+  end
+
+  test "user_idが指定されていない" do
+    @list_member.user_id = nil
+    assert_not @list_member.valid?
+  end
+
+  test "list_idが指定されていない" do
+    @list_member.list_id = nil
+    assert_not @list_member.valid?
+  end
+
 end
