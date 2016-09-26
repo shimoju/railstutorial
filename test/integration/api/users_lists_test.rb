aea9a36323c68951f39fe631a464fee755d0e04a
ogidow <syuta.ogido@gmail.com>
2016-09-26 12:37:41 +0900

    レスポンスに自分が持っているリストが含まれていることをテスト

--- test/integration/api/users_lists_test.rb
+++ test/integration/api/users_lists_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class Api::UsersListsTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:michael)
+    token = @user.generate_jwt
+    @headers = {"Authorization" => "Bearer #{token}"}
+  end
+
+  test "リスト一覧を取得" do
+    list = lists(:friend)
+    @user.lists.build(list).save
+
+    get api_lists_path, {}, @headers
+    assert_response :ok
+
+    assert response_json[:lists].select{|my_list| my_list.name == list.name}
+  end
 end
