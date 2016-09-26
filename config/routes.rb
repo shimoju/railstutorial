99b82d971f20d70d8d8b627fbdee98d5f36018bb
ogidow <syuta.ogido@gmail.com>
2016-09-26 14:18:58 +0900

    どこかで入り込んだ空行を削除

--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,6 @@ Rails.application.routes.draw do
   namespace :api, {format: 'json'} do
     resources :users do
       get :microposts
-
     end
     post 'auth' => 'sessions#create'
     resources :microposts, only: [:create, :destroy]
