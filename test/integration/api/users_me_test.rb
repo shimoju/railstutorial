f506dc7f0b0ab9a1505a669f767eb04a4be01c06
ogidow <syuta.ogido@gmail.com>
2016-09-28 17:31:48 +0900

    インテグレーションテスト

---
+++ test/integration/api/users_me_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class Api::UsersMeTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+    token = @user.generate_jwt
+    @headers = {"Authorization" => "Bearer #{token}"}
+  end
+
+  test "自分の情報を取得" do
+    get api_users_me_path, {}, @headers
+    assert_response :ok
+
+    assert_equal @user[:name], response_json[:user][:name]
+    assert_equal @user[:email], response_json[:user][:email]
+
+    assert_equal @user.following.count, response_json[:user][:following_count]
+    assert_equal @user.followers.count, response_json[:user][:followers_count]
+
+  end
+
+end
