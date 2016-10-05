4445b8fddc55c817450b4fd20d5b1d798af82cb1
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-10-05 12:01:11 +0900

    JSONレスポンスがホスト名を含むURLになるようにasset_hostを設定する

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,5 +1,5 @@
-if Rails.env.production?
-  CarrierWave.configure do |config|
+CarrierWave.configure do |config|
+  if Rails.env.production?
     config.aws_credentials = {
       access_key_id: ENV['BAYT_KEY'],
       secret_access_key: ENV['BAYT_SECRET'],
@@ -10,5 +10,8 @@ if Rails.env.production?
     config.asset_host = ENV['BAYT_ASSET_URL']
     config.aws_bucket = 'sandbox'
     config.aws_acl = 'public-read'
+  else
+    # production以外でもURLにホスト名が含まれるようにしておく(APIのJSONレスポンスで必要)
+    config.asset_host = 'http://localhost:3000'
   end
 end
